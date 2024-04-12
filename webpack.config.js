de0dcceef3c8e8ca959c647d0284c6fe3afd46ef
김 영창 <yckim@daolcompany.com>
2024-04-12 15:50:37 +0900

    version2

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '2048 Game',
+            title: 'Aws Service test',
         })
     ],
     devServer: {
